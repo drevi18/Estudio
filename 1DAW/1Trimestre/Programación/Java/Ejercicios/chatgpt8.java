cc0bb8ab8a58c430a9b4bbc0802dd1b95dc8168b
drevi18 <sara.gomtorr@gmail.com>
2024-10-07 18:44:01 +0200

    act

---
+++ 1DAW/1Trimestre/Programación/Java/Ejercicios/chatgpt8.java
@@ -0,0 +1,8 @@
+
+import java.util.Scanner;
+
+public class chatgpt8 {
+    public static void main(String[] args) {
+        Scanner tec= new Scanner(System.in);
+    }
+}
---
+++ 1DAW/1Trimestre/Programación/Java/Ejercicios/ejercicioextraclase.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class ejercicioextraclase {
+    public static void main(String[] args) {
+        Scanner tec= new Scanner(System.in);
+
+    //Un laboratorio químico español ha empezado a colaborar con un empresa británica de suministros por 
+    //lo que necesita un programa que pase las medidas de capacidad (volumen) tomadas en el sistema británico 
+    //o imperial (barriles, galones, cuartos, pintas, gills y onzas líquidas imperiales o británicas) a litros. Realiza dicho programa. 
+    //xLa tabla de equivalencias sería: Barriles Galones Cuartos Pintas Gills Onzas líquidas Litros 1 35 140 280 1120 5600 159,11315
+
+    float
+    }
+}
