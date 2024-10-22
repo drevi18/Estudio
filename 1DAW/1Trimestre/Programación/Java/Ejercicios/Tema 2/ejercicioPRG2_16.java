6bdf3c9b21b04c436d8d7f4fa5ac3c2dfc1284de
drevi18 <sara.gomtorr@gmail.com>
2024-10-22 21:14:54 +0200

    22oct

---
+++ 1DAW/1Trimestre/Programación/Java/Ejercicios/Tema 2/ejercicioPRG2_16.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class ejercicioPRG2_16 {
+    public static void main(String[] args) {
+        Scanner tec= new Scanner(System.in);
+        //Acepta 10 números enteros y muestra su suma.
+
+        int num,i,sum=0;
+
+        for (i=0;i<=10;i++){
+            System.out.println("Introduce un número entero:");
+            num= tec.nextInt();
+            sum= sum+num;
+        }
+
+        System.out.println("La suma de los 10 números es:"+sum);
+    }
+}
