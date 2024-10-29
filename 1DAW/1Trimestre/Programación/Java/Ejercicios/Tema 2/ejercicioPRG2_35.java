e2c6a95bd84457806fbee12c3c393d33bf19bed4
drevi18 <sara.gomtorr@gmail.com>
2024-10-29 20:32:57 +0100

    oct

--- 1DAW/1Trimestre/Programación/Java/Ejercicios/Tema 2/ejercicioPRG2_35.java
+++ 1DAW/1Trimestre/Programación/Java/Ejercicios/Tema 2/ejercicioPRG2_35.java
@@ -10,14 +10,39 @@ public class ejercicioPRG2_35 {
         //Se supondrá que febrero tiene siempre 28 días.
 
 
-        int d,m,a,fech;
+        int d,m,a,fech,sum;
         System.out.println("Introduce el día:");
         d= tec.nextInt();
+
+        while (d<0 || d>31){
+            System.out.println("Introduce el día:");
+            d= tec.nextInt();
+        }
+        
         System.out.println("Introduce el mes:");
         m= tec.nextInt();
+        while (m<0||m>12){
+        System.out.println("Introduce el mes:");
+        m= tec.nextInt();
+        }
+
         System.out.println("Introduce el año:");
         a= tec.nextInt();
+        while (a<0||a>2024){
+            System.out.println("Introduce el año:");
+            a= tec.nextInt();
+        }
 
-        fech= a*100000000+m*10000+d*100;
+        d=d+1;
+        if (d>31) {
+            m=m+1;
+            d=0;
+        }
+         if (m>12){
+                a=a+1;
+                m=0;
+        }
+        System.out.println(d+"/"+m+"/"+a);
+        
     }
 }
